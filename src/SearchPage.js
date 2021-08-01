304d92f222c06acdee2a014ffb823c5e4cec0059
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-08-01 18:07:04 +0100

    refactor: show no results on search page when input is cleared

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -36,18 +36,13 @@ class SearchPage extends React.Component {
               Close
             </Link>
             <div className="search-books-input-wrapper">
-              {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
               <input
                 type="search"
                 placeholder="Search by title or author"
                 onChange={(event) => {
+                  this.setState({
+                    books: []
+                  })
                   event.target.value !== "" && this.searchBooks(event.target.value);
                 }}
               />
@@ -61,7 +56,9 @@ class SearchPage extends React.Component {
                 onChangeShelf={this.props.onChangeShelf}
               />
             ) : (
-              <h2 className="bookshelf-title">No Search Results</h2>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">No Search Results</h2>
+              </div>
             )}
           </div>
         </div>
