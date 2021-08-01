7a282849057d59c9b94fbb8f5f1d06dd9f764806
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-08-01 16:34:30 +0100

    feat: add links for search page using react router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI';
 import './App.css'
 import Book from "./components/Book";
+import { Route, Link } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: [],
     currentlyReading: [],
     wantToRead: [],
@@ -47,10 +41,16 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+
+        <Route path="/search" render={() => (
           <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <Link
+                className="close-search"
+                to="/"
+              >
+                Close
+              </Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -68,7 +68,9 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )} />
+
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -123,10 +125,15 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <Link
+                className="open-search-button"
+                to="/search"
+              >
+                Add a book
+              </Link>
             </div>
           </div>
-        )}
+        )} />
       </div>
     )
   }
