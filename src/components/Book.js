eee97cec89e274411314c05d45138b5b7df1ebdd
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-08-01 14:09:42 +0100

    feat: create book component

---
+++ src/components/Book.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = (props) => {
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                    <select defaultValue={props.shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
+            </div>
+            <div className="book-title">{props.title}</div>
+            {props.authors.map(author =>
+                <div
+                    key={author}
+                    className="book-authors">
+                    {author}
+                </div>
+            )}
+        </div>
+    )
+}
+
+Book.propTypes = {
+    title: PropTypes.string,
+    authors: PropTypes.array,
+    thumbnail: PropTypes.string,
+    shelf: PropTypes.string
+}
+
+export default Book;
