b7b5ef337f21a02c18f5a09ee33feb8bdc07030b
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-08-02 17:16:53 +0100

    feat: add debounce for search input field

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,73 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as BooksAPI from './BooksAPI';
 import BookShelf from './components/BookShelf';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-class SearchPage extends React.Component {
+const SearchPage = (props) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: [],
-    };
+  const { onChangeShelf, books } = props;
+  const [state, setState] = useState(
+    {
+      searchResults: [],
+      query: "",
+      message: ""
+    }
+  );
+
+  let timer;
+
+  const setQuery = (query) => {
+    setState(prevState => {
+      return { ...prevState, query: query };
+    })
   }
 
-  searchBooks(query) {
-    console.log(query);
-    BooksAPI.search(query)
+  const debounceInput = (event) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      setState(prevState => {
+        return { ...prevState, message: "searching please wait ..." };
+      });
+      searchBooks();
+    }, 500)
+  }
+
+  const handleSearchInput = () => {
+    clearTimeout(timer);
+    setState(prevState => {
+      return { ...prevState, message: "you are typing ..." };
+    })
+  }
+
+  const searchBooks = () => {
+    BooksAPI.search(state.query)
       .then((response) => {
-        this.setState({
-          books: response
+        setState(prevState => {
+          return { ...prevState, searchResults: response, message: "" }
         })
       })
   }
 
-  render() {
-    return (
-      <div className="app">
 
-        <div className="search-books">
-          <div className="search-books-bar">
-            <Link
-              className="close-search"
-              to="/"
-            >
-              Close
-            </Link>
-            <div className="search-books-input-wrapper">
-              <input
-                type="search"
-                placeholder="Search by title or author"
-                onChange={(event) => {
-                  this.setState({
-                    books: []
-                  })
-                  event.target.value !== "" && this.searchBooks(event.target.value);
-                }}
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            {this.state.books.length ? (
-              <BookShelf
-                title="Search Results"
-                books={this.state.books}
-                onChangeShelf={this.props.onChangeShelf}
-              />
-            ) : (
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">No Search Results</h2>
-              </div>
-            )}
+  return (
+    <div className="app">
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link
+            className="close-search"
+            to="/"
+          >
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="search"
+              placeholder="Search by title or author"
+              onChange={(event) => setQuery(event.target.value)}
+              onKeyPress={(event) => handleSearchInput(event)}
+              onKeyUp={() => debounceInput()}
+            />
           </div>
         </div>
+        <div className="search-books-results">
+          {state.searchResults?.length ? (
+            <BookShelf
+              title="Search Results"
+              books={state.searchResults}
+              onChangeShelf={onChangeShelf}
+            />
+          ) : (
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">{state.message || "No Search Results"}</h2>
+            </div>
+          )}
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 SearchPage.propTypes = {
+  books: PropTypes.array.isRequired,
   onChangeShelf: PropTypes.func.isRequired,
 }
 
